c4630902a6d3bf5c1e89f2eee54a386487c31b40
romarikid <57034453+romarikid@users.noreply.github.com>
2020-01-19 18:46:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,16 +7,16 @@
 	$password = 'root';
 	$dbName = 'test';
 
-	$link = mysql_connect($host, $user, $password, $dbName);
-	mysqli_query($link, "SET NAMES 'utf8'");
-	$query = "SELECT * FROM pages";
-	$result = mysqli_query($link, $query) or die(mysql_error($link));
-	for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
-
-		foreach ($data as $page) {
-
-		}
-
+	//$link = mysql_connect($host, $user, $password, $dbName);
+	//mysqli_query($link, "SET NAMES 'utf8'");
+	//$query = "SELECT * FROM pages";
+	//$result = mysqli_query($link, $query) or die(mysql_error($link));
+	//for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+//
+//		foreach ($data as $page) {
+//
+//		}
+//
 		$title = 'admin main page';
 		$content = '';
 
