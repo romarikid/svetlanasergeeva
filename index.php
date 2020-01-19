b026a4476e506b796062aaa57ffaa78f1d135c1f
romarikid <57034453+romarikid@users.noreply.github.com>
2020-01-19 19:04:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="text/css?v=1">
+	<title><!--?= $title ?--></title>
+</head>
+<body>
+<div id="wrapper">
+	<header>
+		header
+	</header>
+	<main>
+		<!--<?= $content ?>-->
+	</main>
+	<footer>
+		footer
+	</footer>
+</div>
+</body>
+</html>
